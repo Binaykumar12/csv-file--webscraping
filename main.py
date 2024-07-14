aa1869b6cb0a957adc67340f3e5a55f3153d7a42
Binaykumar12 <binaykumarkurmi78@gmail.com>
2024-07-14 21:53:59 +0545

    .

--- main.py
+++ main.py
@@ -7,24 +7,19 @@ import pandas as pd
 # URL of the page to scrape
 url = "https://quotes.toscrape.com/"
 
-# Define headers to mimic a browser request
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.5"
 }
 
-# Make an HTTP GET request to the URL with headers
 response = requests.get(url, headers=headers)
 
-# Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(response.text, "html.parser")
 
-# Extract and print quotes
 quotes = soup.find_all("span", class_="text")
 for quote in quotes:
     print(quote.text)
 
-# Extract and print authors
 authors = soup.find_all("small", class_="author")
 for author in authors:
     print(author.text)
@@ -38,7 +33,6 @@ data = {
     
 }
 df = pd.DataFrame(data)
-
 excel_filename = "quotes.xlsx"
 df.to_excel(excel_filename, index=False)
 
